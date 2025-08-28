f0aab5d943b7001957766426bfc1a35af695e489
Pratik Jagad <pratikjagad20@gmail.com>
2025-08-27 23:07:41 -0400

    Python file Multiply added

--- Calculator.py
+++ Calculator.py
@@ -6,6 +6,11 @@ def subtract(a, b):
     """Return the difference of a and b."""
     return a - b
 
+def multiply(a, b):
+    """Return the multiply of a and b."""
+    return a * b
+
 if __name__ == "__main__":
     print("Addition:", add(5, 3))
     print("Subtraction:", subtract(5, 3))
+    print("Multiply:", multiply(5, 3))
