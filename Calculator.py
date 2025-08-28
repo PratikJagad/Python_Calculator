00e7d89aae4c8c6ab4d15b3877d11cb26815dd4b
Pratik Jagad <pratikjagad20@gmail.com>
2025-08-27 22:57:41 -0400

    Python file changed

--- Calculator.py
+++ Calculator.py
@@ -2,10 +2,10 @@ def add(a, b):
     """Return the sum of a and b."""
     return a + b
 
-# def subtract(a, b):
-#     """Return the difference of a and b."""
-#     return a - b
+def subtract(a, b):
+    """Return the difference of a and b."""
+    return a - b
 
 if __name__ == "__main__":
     print("Addition:", add(5, 3))
-#    print("Subtraction:", subtract(5, 3))
+    print("Subtraction:", subtract(5, 3))
